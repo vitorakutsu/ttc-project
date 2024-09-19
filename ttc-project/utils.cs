9cf7f3646d2907a01ccece901488261558c7a66b
vitorakutsu <vitorakutsu@gmail.com>
2024-09-18 23:13:33 -0300

    feat: added utils

---
+++ ttc-project/utils.cs
@@ -0,0 +1,42 @@
+﻿namespace ttc_project
+{
+    class utils
+    {
+        public class withoutDMA
+        {
+            public static bool isBlack(Color color)
+            {
+                return color.R == 0 && color.G == 0 && color.B == 0;
+            }
+
+            public static bool isWhite(Color color)
+            {
+                return color.R == 255 && color.G == 255 && color.B == 255;
+            }
+
+            public static bool isRed(Color color)
+            {
+                return color.R == 255 && color.G == 0 && color.B == 0;
+            }
+        }
+
+        public class withDMA
+        {
+            public static unsafe bool isBlack(byte* pixel)
+            {
+                return pixel[0] == 0 && pixel[1] == 0 && pixel[2] == 0;
+            }
+
+
+            public static unsafe bool isWhite(byte* pixel)
+            {
+                return pixel[0] == 255 && pixel[1] == 255 && pixel[2] == 255;
+            }
+
+            public static unsafe bool isRed(byte* pixel)
+            {
+                return pixel[0] == 0 && pixel[1] == 0 && pixel[2] == 255;
+            }
+        }
+    }
+}
